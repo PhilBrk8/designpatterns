4ac7be2a61af4b10b18b481ab73b7dacbd8e66a8
Phil Brk8 <92030164+PhilBrk8@users.noreply.github.com>
2024-02-22 23:04:43 +0100

    added description

--- creational/factoryMethod.ts
+++ creational/factoryMethod.ts
@@ -1,5 +1,15 @@
-// https://refactoring.guru/design-patterns/factory-method/typescript/example
-// Factory method is a creational design pattern which solves the problem of creating product objects without specifying their concrete classes.
+// https://refactoring.guru/design-patterns/factory-method/
+// Factory Method is a creational design pattern that provides an interface for creating objects in a superclass, but allows subclasses to alter the type of objects that will be created.
+
+// Problem:
+// Imagine that you’re creating a logistics management application. The first version of your app can only handle transportation by trucks, so the bulk of your code lives inside the Truck class.
+// After a while, your app becomes pretty popular. Each day you receive dozens of requests from sea transportation companies to incorporate sea logistics into the app.
+// Adding Ships into the app would require making changes to the entire codebase. Moreover, if later you decide to add another type of transportation to the app, you will probably need to make all of these changes again.
+// As a result, you will end up with pretty nasty code, riddled with conditionals that switch the app’s behavior depending on the class of transportation objects.
+
+// Solution:
+// The Factory Method pattern suggests that you replace direct object construction calls (using the new operator) with calls to a special factory method.
+// Don’t worry: the objects are still created via the new operator, but it’s being called from within the factory method. Objects returned by a factory method are often referred to as products.
 
 /**
  * The Creator class declares the factory method that is supposed to return an
